67fe5b06cb5ab77cbd61c4fcfc9b25b6a9a4cd81
hanhpuu <35247425+hanhpuu@users.noreply.github.com>
2018-01-23 22:32:09 +0700

    add default time and distance

--- app/Http/Models/Agenda.php
+++ app/Http/Models/Agenda.php
@@ -4,71 +4,99 @@ namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Agenda extends Model
-{
+class Agenda extends Model {
 
-	public static function getAllAgendas()
-	{
-		return [
-			[
-				'name' => 'Dâu chân đầu tiên',
-				'data' => [
-						[
-						'name' => 'Xã Lát',
-						'lat' => 12.113155,
-						'lng' => 108.411023,
-						'hour' => 1
-					],
-						[
-						'name' => 'Langbiang',
-						'lat' => 12.095916,
-						'lng' => 108.413252,
-						'hour' => 2
-					],
-						[
-						'name' => 'Ma rừng lữ quán',
-						'lat' => 12.011218,
-						'lng' => 108.347851,
-						'hour' => 1
-					],
-						[
-						'name' => 'Làng cù lần',
-						'lat' => 12.028467,
-						'lng' => 108.367633,
-						'hour' => 2
-					]
-				]
-			],
-			[
-				'name' => 'Ngoại ô Đà Lạt',
-				'data' => [
-						[
-						'name' => 'Thác Voi',
-						'lat' => 11.823414,
-						'lng' => 108.335087,
-						'hour' => 1
-					],
-						[
-						'name' => 'Chùa Linh Ấn Tự',
-						'lat' => 11.824717,
-						'lng' => 108.334160,
-						'hour' => 2
-					],
-						[
-						'name' => 'Trại nuôi dế',
-						'lat' => 11.882753,
-						'lng' => 108.335689,
-						'hour' => 1
-					],
-						[
-						'name' => 'Dệt lụa tơ tằm',
-						'lat' => 11.824341,
-						'lng' => 108.339325,
-						'hour' => 2
-					]
-				]
-			]
-		];
-	}
+    public static function getAllAgendas()
+    {
+	return [
+	    [
+		'name' => 'Dấu chân đầu tiên',
+		'data' => [
+		    [
+			'name' => 'Làng cù lần',
+			'lat' => 12.028467,
+			'lng' => 108.367633,
+			'hour' => 2
+		    ],
+		    [
+			'name' => 'Xã Lát',
+			'lat' => 12.113155,
+			'lng' => 108.411023,
+			'hour' => 1
+		    ],
+		    [
+			'name' => 'Langbiang',
+			'lat' => 12.095916,
+			'lng' => 108.413252,
+			'hour' => 2
+		    ],
+		    [
+			'name' => 'Ma rừng lữ quán',
+			'lat' => 12.011218,
+			'lng' => 108.347851,
+			'hour' => 1
+		    ]
+		]
+	    ],
+	    [
+		'name' => 'Ngoại ô Đà Lạt',
+		'data' => [
+		    [
+			'name' => 'Làng Hoa Vạn Thành',
+			'lat' => 11.945704,
+			'lng' => 108.418235,
+			'hour' => 0.5
+		    ],
+		    [
+			'name' => 'Hoa Sơn Điền Trang',
+			'lat' => 11.934284,
+			'lng' => 108.372573,
+			'hour' => 1.5
+		    ],
+		    [
+			'name' => 'Mê Linh Garden Coffee',
+			'lat' => 11.899012,
+			'lng' => 108.352832,
+			'hour' => 1
+		    ],
+		    [
+			'name' => 'Khu Du Lịch Thác Voi',
+			'lat' => 11.824926,
+			'lng' => 108.321761,
+			'hour' => 2
+		    ]
+		]
+	    ],
+	    [
+		'name' => 'Vùng chè và Cà phê',
+		'data' => [
+		    [
+			'name' => 'Hầm rượu vang',
+			'lat' => 11.942855,
+			'lng' => 108.429335,
+			'hour' => 0.5
+		    ],
+		    [
+			'name' => 'Nhà máy trà Atiso',
+			'lat' => 11.955955,
+			'lng' => 108.477057,
+			'hour' => 1
+		    ],
+		    [
+			'name' => 'Cánh đồng hoa Cẩm tú cầu',
+			'lat' => 11.947222,
+			'lng' => 108.518771,
+			'hour' => 1
+		    ],
+		    [
+			'name' => 'Cầu đất Farm: trà & cafe',
+			'lat' => 11.884908,
+			'lng' => 108.569754,
+			'hour' => 2
+		    ]
+		]
+	    ]
+	];
+    }
 
 }
--- resources/views/front/demo/chooseRoute.blade.php
+++ resources/views/front/demo/chooseRoute.blade.php
@@ -53,10 +53,26 @@ use App\Http\Constant\Common as CommonConst;
 			    @foreach($agendas as $i => $agenda)    
 			    @if(!$i)
 			    <div class="tab-pane active" id="portlet_tab_{{$i}}">
+				<div>
+				    <ul><b>Tổng quãng đường dự kiến:</b> 89.0 km</ul>
+				    <ul><b>Tổng thời gian dự kiến:</b> 4 giờ 8 phút </ul>
+				</div>
 				@else
+
 				<div class="tab-pane" id="portlet_tab_{{$i}}">
 				    @endif
-					    <!--<div> <input type='checkbox' name='Agenda[{{$agenda['name']}}]' class='agenda'> </div>-->
+				    @if($i==1)
+				    <div>
+					<ul><b>Tổng quãng đường dự kiến:</b> 55.5 km</ul>
+					<ul><b>Tổng thời gian dự kiến:</b> 1 giờ 43 phút </ul>
+				    </div>
+				    @elseif($i==2)
+				    <div>
+					<ul><b>Tổng quãng đường dự kiến:</b> 58.1 km </ul>
+					<ul><b>Tổng thời gian dự kiến:</b> 1 giờ 55 phút </ul>
+				    </div>
+				    @endif	   
+	 <!--<div> <input type='checkbox' name='Agenda[{{$agenda['name']}}]' class='agenda'> </div>-->
 
 				    <div id="map_{{$i}}" class="map"></div>	
 				</div>
@@ -66,7 +82,7 @@ use App\Http\Constant\Common as CommonConst;
 
 				<form method="POST">
 				    <input type='hidden' name='_token' value="{{csrf_token()}}">
-				    <input type='hidden' name='finalData' id='finalData'>
+				    <input type='hidden' name='routes' id='routes'>
 				    <button type="submit" value="Xem lịch trình" class="btn btn-primary">Xem lịch trình</button>
 				</form>
 			    </div>
@@ -85,21 +101,21 @@ use App\Http\Constant\Common as CommonConst;
     <!--<script src="https://maps.googleapis.com/maps/api/js"></script>-->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEqDMXEY3pAjm_G5utfzA2ukLvJk-1I7Q"></script>
     <script>
-	var locations = '';
-	var myJSON = JSON.parse('{!! $agenda_json !!}');
-	var lat = {{$location['lat']}};
-	var lng = {{$location['lng']}};
-	$(document).ready(function(){
-	    $("form").submit(function(){
-		var index = $('.ul-map').find('.active').find('a').attr('data-index');
-		var data = myJSON[index]['data'];
-		var stringData = JSON.stringify(data);
-		document.getElementById("finalData").value = stringData;
-	    });
-	});
+var locations = '';
+var myJSON = JSON.parse('{!! $agenda_json !!}');
+var lat = {{$location['lat']}};
+var lng = {{$location['lng']}};
+$(document).ready(function(){
+$("form").submit(function(){
+var index = $('.ul-map').find('.active').find('a').attr('data-index');
+var data = myJSON[index]['data'];
+var stringData = JSON.stringify(data);
+document.getElementById("routes").value = stringData;
+});
+});
     </script>
     <script type="text/javascript" src="/js/demo/gmap.js"></script>
-	<script type="text/javascript" src="/js/demo/chooseRoute.js"></script>
+    <script type="text/javascript" src="/js/demo/chooseRoute.js"></script>
     @endsection
 
     @section('css')
