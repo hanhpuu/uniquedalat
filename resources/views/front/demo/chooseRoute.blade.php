8c2cdc6f331b655ac8e0e5fdab561ecdf8493e7c
Dzung Do <kiemt95@gmail.com>
2018-01-25 09:48:29 +0700

    fix bugs

--- resources/views/front/demo/chooseRoute.blade.php
+++ resources/views/front/demo/chooseRoute.blade.php
@@ -7,23 +7,12 @@ use App\Http\Constant\Common as CommonConst;
 @extends('layouts.app')
 
 @section('content')
-<ul class="breadcrumb">
-    <li><a href="index.html">Home</a></li>
-    <li><a href="#">Pages</a></li>
-    <li class="active">Login</li>
-</ul>
 <!-- BEGIN SIDEBAR & CONTENT -->
 <div class="row margin-bottom-40">
-    <!-- BEGIN SIDEBAR -->
-    <div class="sidebar col-md-3 col-sm-3">
-		<div class="margin-bottom-10"><button type="button" class="btn green">Green</button></div>
-		<div class="margin-bottom-10"><button type="button" class="btn green">Green</button></div>
-		<div class="margin-bottom-10"><button type="button" class="btn green">Green</button></div>
-    </div>
     <!-- END SIDEBAR -->
 
     <!-- BEGIN CONTENT -->
-    <div class="col-md-9 col-sm-9">
+    <div class="col-md-12 col-sm-12">
 		<div class="content-form-page">
 			<div class="row">
 				<?php
@@ -46,9 +35,9 @@ use App\Http\Constant\Common as CommonConst;
 							@foreach($agendas as $i => $agenda)	    
 							@if(!$i)
 							<li class="active">
-								@else
+							@else
 							<li>
-								@endif						
+							@endif						
 								<a href="#portlet_tab_{{$i}}" data-toggle="tab" aria-expanded="true" data-index="{{$i}}">
 									{{$agenda['name']}}
 								</a>
@@ -67,54 +56,35 @@ use App\Http\Constant\Common as CommonConst;
 									<ul><b>Tổng quãng đường dự kiến:</b> 89.0 km</ul>
 									<ul><b>Tổng thời gian dự kiến:</b> 10 giờ  </ul>
 								</div>
+								<div id="map_{{$i}}" class="map"></div>
+							</div>
 							@else
 							<div class="tab-pane" id="portlet_tab_{{$i}}">
+								@if($i==1)
+									<div>
+										<ul><b>Tổng quãng đường dự kiến:</b> 55.5 km</ul>
+										<ul><b>Tổng thời gian dự kiến:</b> 8 giờ </ul>
+									</div>
+								@elseif($i==2)
+									<div>
+										<ul><b>Tổng quãng đường dự kiến:</b> 58.1 km </ul>
+										<ul><b>Tổng thời gian dự kiến:</b> 7 giờ </ul>
+									</div>
+								@endif
+								<div id="map_{{$i}}" class="map"></div>
+							</div>
 							@endif
-							
-							@if($i==1)
-								<div>
-									<ul><b>Tổng quãng đường dự kiến:</b> 55.5 km</ul>
-									<ul><b>Tổng thời gian dự kiến:</b> 8 giờ </ul>
-								</div>
-							@elseif($i==2)
-								<div>
-									<ul><b>Tổng quãng đường dự kiến:</b> 58.1 km </ul>
-									<ul><b>Tổng thời gian dự kiến:</b> 7 giờ </ul>
-								</div>
-							@endif	   
-					 <!--<div> <input type='checkbox' name='Agenda[{{$agenda['name']}}]' class='agenda'> </div>-->
-
-							</div>
-							@endforeach
-							<div class="portlet-body">
-								<div class="tab-content">
-									@foreach($agendas as $i => $agenda)    
-									@if(!$i)
-									<div class="tab-pane active" id="portlet_tab_{{$i}}">
-										@else
-										<div class="tab-pane" id="portlet_tab_{{$i}}">
-											@endif
-												<!--<div> <input type='checkbox' name='Agenda[{{$agenda['name']}}]' class='agenda'> </div>-->
-
-											<div id="map_{{$i}}" class="map"></div>	
-										</div>
-										@endforeach
-									</div>
-									<div class="col-lg-10 col-md-offset-2 padding-left-0 padding-top-20">
-									</div>
-								</div>
-							</div>					
-							</div>
-						</div>
-						<form method="POST">
-								<input type='hidden' name='_token' value="{{csrf_token()}}">
-								<input type="hidden" name="date_part" value="{{$current_date. ' '. $current_period}}">
-								<input type='hidden' name='routes' id='routes'>
-								<button type="submit" class="btn btn-primary">Chọn lịch trình</button>
-						</form>
-					</div>
+							@endforeach		
+						</div>	
+					</div>					
 					<!-- END CONTENT -->
 				</div>
+				<form method="POST">
+						<input type='hidden' name='_token' value="{{csrf_token()}}">
+						<input type="hidden" name="date_part" value="{{$current_date. ' '. $current_period}}">
+						<input type='hidden' name='routes' id='routes'>
+						<button type="submit" class="btn btn-primary">Chọn lịch trình</button>
+				</form>
 			</div>
 		</div>
 	</div>
--- resources/views/front/demo/index.blade.php
+++ resources/views/front/demo/index.blade.php
@@ -1,23 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<ul class="breadcrumb">
-    <li><a href="index.html">Home</a></li>
-    <li><a href="#">Pages</a></li>
-    <li class="active">Login</li>
-</ul>
 <!-- BEGIN SIDEBAR & CONTENT -->
 <div class="row margin-bottom-40">
-    <!-- BEGIN SIDEBAR -->
-    <div class="sidebar col-md-3 col-sm-3">
-	<div class="margin-bottom-10"><button type="button" class="btn green">Green</button></div>
-	<div class="margin-bottom-10"><button type="button" class="btn green">Green</button></div>
-	<div class="margin-bottom-10"><button type="button" class="btn green">Green</button></div>
-    </div>
-    <!-- END SIDEBAR -->
 
     <!-- BEGIN CONTENT -->
-    <div class="col-md-9 col-sm-9">
+    <div class="col-md-12 col-sm-12">
 	<div class="content-form-page">
 	    <div class="row">
                 <div class="col-md-12 col-sm-12 pull-left">
